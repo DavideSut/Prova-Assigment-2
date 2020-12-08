573ee56b49aeb78075d47ef9d0f80f29b2a274ff
Davide Sut <davide.sut@studenti.unipd.it>
2020-12-08 12:33:43 +0100

    Modificato RestaurantBillException

--- icecream-shop-manager/src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
+++ icecream-shop-manager/src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
@@ -11,8 +11,6 @@ public class RestaurantBillException extends Exception {
     public RestaurantBillException(String errInfo){
         if(errInfo == "To many items"){
             System.out.println("Items must be < 30");
-        }else{
-            System.out.println("Generic error");
         }
         errInfo = this.errInfo;
     }
